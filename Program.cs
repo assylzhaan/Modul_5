3fb0df4cafb874a71928063d6d3337c115bdda5e
Assyl <to1egeno8naa@gmail.com>
2023-10-23 07:57:25 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ExceptionDemo
+    {
+        public void GenerateException()
+        {
+            Console.WriteLine("Метод начал выполнение.");
+            throw new ApplicationException("Произошла ошибка!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало программы.");
+
+            try
+            {
+                ExceptionDemo demo = new ExceptionDemo();
+                CallGenerateException(demo);
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Завершение программы.");
+            }
+
+        }
+
+        public static void CallGenerateException(ExceptionDemo demo)
+        {
+            Console.WriteLine("Вызываем метод GenerateException() из CallGenerateException().");
+            demo.GenerateException();
+        }
+    }
+}
